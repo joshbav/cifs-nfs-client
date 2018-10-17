be3f99cf3b9644cdaa4a46e8fb849732155c9fff
“joshbav” <josh@Joshs-Mac.frontierlocal.net>
2018-10-16 18:19:46 -0700

    scripted commit 10-16-18-18-19-46

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo
 # All files to automatically be added
 git add *
 git config user.name “joshbav”
-git commit -m "scripted commit $(date +%m-%d-%y)"
+git commit -m "scripted commit $(date +%m-%d-%y-%H-%M-%S)"
 git push -u origin master
 
 
